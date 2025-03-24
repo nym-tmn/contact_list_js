e8cfb3ef0d03d8947eae2a6f5179e6df8e25c7e9
nym-tmn <nymtmn8690@gmail.com>
2025-03-25 00:35:48 +0500

    Added functionality for deleting a single contact

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const countersCollection = new Map();
 document.querySelectorAll('.contact-list__item')
 	.forEach((elem) => {
 
-	countersCollection.set(elem.dataset.item, 0);
+		countersCollection.set(elem.dataset.item, 0);
 
-	const countElement = document.createElement('div');
-	countElement.innerText = countersCollection.get(elem.dataset.item);
-	countElement.classList.add('contact-list__counter');
+		const countElement = document.createElement('div');
+		countElement.innerText = countersCollection.get(elem.dataset.item);
+		countElement.classList.add('contact-list__counter');
 		elem.append(countElement);
-		
+
 		const contactsContainer = document.createElement('div');
 		contactsContainer.classList.add('contacts-container');
 		contactsContainer.hidden = true;
@@ -19,8 +19,27 @@ document.querySelectorAll('.contact-list__item')
 document.querySelectorAll('.contact-list').forEach((elem) => {
 	elem.addEventListener('click', (event) => {
 
-		const contactListItem = event.target.closest(`.contact-list__item`);
-		contactListItem.nextElementSibling.hidden = !contactListItem.nextElementSibling.hidden;
+		const contactListItem = event.target.closest('.contact-list__item');
+		if (event.target === contactListItem) {
+			contactListItem.nextElementSibling.hidden = !contactListItem.nextElementSibling.hidden;
+			return;
+		}
+
+		const deleteButton = event.target.closest('.contact__button-img');
+		if (event.target === deleteButton) {
+			const currentItem = event.target.parentElement.parentElement.parentElement.previousElementSibling;
+			const currentContact = event.target.parentElement.parentElement;
+
+			countersCollection.forEach((value, key) => {
+				if (key === currentItem.dataset.item) {
+					countersCollection.set(key, --value);
+					if (value === 0) currentItem.classList.remove('contact-list__item_active');
+				}
+			})
+
+			currentItem.firstElementChild.innerText = `${countersCollection.get(currentItem.dataset.item)}`
+			currentContact.remove();
+		}
 	})
 })
 
@@ -42,12 +61,11 @@ form.addEventListener('submit', (event) => {
 	<div>Last name: ${contact.lastName}</div>
 	<div>Phone: ${contact.phone}</div>
 	</div>
-	<button class="contact__button" type="button"><img src="./img/delete_contact_card_icon.svg" alt="Delete current contact button"></button>
+	<button class="contact__button" type="button"><img class="contact__button-img" src="./img/delete_contact_card_icon.svg" alt="Delete current contact button"></button>
 	`;
 
 	const currentItem = document.querySelector(`[data-item=${contact.firstName[0].toLowerCase()}]`);
 	currentItem.classList.add('contact-list__item_active');
-
 	currentItem.nextElementSibling.prepend(contactCard);
 
 	countersCollection.forEach((value, key) => {
